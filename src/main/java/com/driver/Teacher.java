f494d6e9f87c4ea0b437c1e1df6672cd93613fb9
farhan.2763 <farhan2763@gmail.com>
2023-10-07 01:01:36 +0530

    done

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -1,4 +1,9 @@
 package com.driver;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 public class Teacher {
 
@@ -10,5 +15,11 @@ public class Teacher {
 
     public Teacher() {
 
+    }
+
+    public Teacher(String name, int numberOfStudents, int age) {
+        this.name = name;
+        this.numberOfStudents = numberOfStudents;
+        this.age = age;
     }
 }
